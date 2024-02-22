28eb175b3835c7d84d16a2c1efd67f3ff591e12b
Simen Mailund Svendsen <simen@erus.no>
2024-02-22 22:52:56 +0100

    lsp

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -9,4 +9,9 @@ require("lazy").setup({
     "nvim-telescope/telescope.nvim", tag = "0.1.4",
     dependencies = { "nvim-lua/plenary.nvim" }
   },
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
 })
