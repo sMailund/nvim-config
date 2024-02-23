2d4ffab4e2698dbfeb0651f71f305fff8bcbead6
Simen Mailund Svendsen <simen@erus.no>
2024-02-23 14:57:35 +0100

    keymaps

--- lua/core/plugin_config/lsp_zero.lua
+++ lua/core/plugin_config/lsp_zero.lua
@@ -10,6 +10,28 @@ lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
   lsp_zero.default_keymaps({buffer = bufnr})
+
+  local opts = {buffer = bufnr}
+
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'gci', vim.lsp.buf.incoming_calls, opts)
+    vim.keymap.set('n', 'gco', vim.lsp.buf.outgoing_calls, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+
 end)
 
 local lspconfig = require('lspconfig')
@@ -43,8 +65,6 @@ require('mason-lspconfig').setup({
       end
   },
 })
-
-require('lspconfig').csharp_ls.setup({})
 
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
