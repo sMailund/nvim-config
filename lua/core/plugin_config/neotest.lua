05cf7c30ca75be186a0324eea7f4ce29eb6cc739
Simen Mailund Svendsen <simen@erus.no>
2024-02-23 23:29:52 +0100

    rustacean and which key, remember to config which key

---
+++ lua/core/plugin_config/neotest.lua
@@ -0,0 +1,6 @@
+require("neotest").setup({
+  adapters = {
+    require("neotest-dotnet"),
+    require('rustaceanvim.neotest')
+  }
+})
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,6 +1,5 @@
 require("lazy").setup({
   "ellisonleao/gruvbox.nvim",
-  "vim-test/vim-test",
   "nvim-tree/nvim-tree.lua",
   "nvim-tree/nvim-web-devicons",
   "nvim-lualine/lualine.nvim",
@@ -18,4 +17,31 @@ require("lazy").setup({
   {'hrsh7th/cmp-nvim-lsp'},
   {'hrsh7th/nvim-cmp'},
   {'L3MON4D3/LuaSnip'},
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "Issafalcon/neotest-dotnet",
+    }
+  },
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^4', 
+    ft = { 'rust' },
+  }
 })
